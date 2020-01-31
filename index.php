aa58a4947a0a82fd70f7ba042e8b693be7387ea1
smpny7 <symphonymode7@icloud.com>
2020-01-31 16:54:18 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="robots" content="noindex">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>情クラサーバ</title>
+//    <link rel="apple-touch-icon" sizes="180x180" href="/read/favicon/apple-touch-icon.png">
+//    <link rel="icon" href="/read/favicon/favicon.ico">
+    <link rel="stylesheet" href="/read/css/reset.css">
+    <link rel="stylesheet" href="/read/css/common.css">
+    <link rel="stylesheet" href="/read/css/home.css">
+</head>
+
+<body>
+    <header>
+        <div class="top">ホーム</div>
+        <a class="home_link" href="/home"><object class="home" type="image/svg+xml" data="/read/img/home.svg"></object></a>
+    </header>
+
+    <main>
+        <div class="block"></div>
+        <div class="home_container">
+            <div class="home_box"></div>
+            <div class="home_title">メンテナンスのお知らせ</div>
+            <div class="home_detail">以下の期間において、アップデートに伴いサーバを一時停止します。...</div>
+            <div class="time_container">
+                <object class="time" type="image/svg+xml" data="/read/img/time.svg"></object>
+                <div class="time_letter">1時間前</div>
+                <div class="time_detail"><a href="#">詳細</a></div>
+            </div>
+        </div>
+
+        <div class="home_container">
+            <div class="home_box"></div>
+            <div class="home_title">メンテナンスのお知らせ</div>
+            <div class="home_detail">以下の期間において、アップデートに伴いサーバを一時停止します。...</div>
+            <div class="time_container">
+                <object class="time" type="image/svg+xml" data="/read/img/time.svg"></object>
+                <div class="time_letter">1時間前</div>
+                <div class="time_detail"><a href="#">詳細</a></div>
+            </div>
+        </div>
+
+        <div class="home_container">
+            <div class="home_box"></div>
+            <div class="home_title">メンテナンスのお知らせ</div>
+            <div class="home_detail">以下の期間において、アップデートに伴いサーバを一時停止します。...</div>
+            <div class="time_container">
+                <object class="time" type="image/svg+xml" data="/read/img/time.svg"></object>
+                <div class="time_letter">1時間前</div>
+                <div class="time_detail"><a href="#">詳細</a></div>
+            </div>
+        </div>
+
+        <div class="block"></div>
+        <div class="pagetop">↑</div>
+    </main>
+
+    <?php include './../read/php/menu.php' ?>
+    <script type="text/javascript" src="/read/js/pagetop.js"></script>
+</body>
+</html>

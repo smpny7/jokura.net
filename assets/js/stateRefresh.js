d28944e4e6ba4bbced16a06d1f4f6e7c5ee0a370
smpny7 <symphonymode7@icloud.com>
2020-02-06 17:57:36 +0900

    online 完成

---
+++ assets/js/stateRefresh.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+    refresh_state_1();
+    refresh_state_2();
+    refresh_state_3();
+    refresh_state_4();
+    refresh_state_5();
+    refresh_state_6();
+    refresh_state_7();
+    refresh_state_8();
+    refresh_state_9();
+    refresh_state_10();
+});
+
+function refresh_state_1(){
+    $('#online_state_1').load('/assets/php/state/Chazuke_0_.php', function(){
+        setTimeout(refresh_state_1,3000);
+    });
+}
+
+function refresh_state_2(){
+    $('#online_state_2').load('/assets/php/state/chishige1217200.php', function(){
+        setTimeout(refresh_state_2,3000);
+    });
+}
+
+function refresh_state_3(){
+    $('#online_state_3').load('/assets/php/state/fkddn123.php', function(){
+        setTimeout(refresh_state_3,3000);
+    });
+}
+
+function refresh_state_4(){
+    $('#online_state_4').load('/assets/php/state/kit130101.php', function(){
+        setTimeout(refresh_state_4,3000);
+    });
+}
+
+function refresh_state_5(){
+    $('#online_state_5').load('/assets/php/state/meromin007.php', function(){
+        setTimeout(refresh_state_5,3000);
+    });
+}
+
+function refresh_state_6(){
+    $('#online_state_6').load('/assets/php/state/RyuMura.php', function(){
+        setTimeout(refresh_state_6,3000);
+    });
+}
+
+function refresh_state_7(){
+    $('#online_state_7').load('/assets/php/state/suzumetengu.php', function(){
+        setTimeout(refresh_state_7,3000);
+    });
+}
+
+function refresh_state_8(){
+    $('#online_state_8').load('/assets/php/state/TUIUS.php', function(){
+        setTimeout(refresh_state_8,3000);
+    });
+}
+
+function refresh_state_9(){
+    $('#online_state_9').load('/assets/php/state/Xx_piroboy_xX.php', function(){
+        setTimeout(refresh_state_9,3000);
+    });
+}
+
+function refresh_state_10(){
+    $('#online_state_10').load('/assets/php/state/YuuYuu523.php', function(){
+        setTimeout(refresh_state_10,3000);
+    });
+}
--- online/index.php
+++ online/index.php
@@ -7,6 +7,8 @@
     <title>オンラインユーザ</title>
     <link rel="stylesheet" href="/assets/css/reset.css">
     <link rel="stylesheet" href="/assets/css/online.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script type="text/javascript" src="/assets/js/stateRefresh.js"></script>
     <?php include '../assets/php/server.php'?>
 </head>
 
@@ -17,16 +19,16 @@
         <img class="background" src="/assets/img/background.jpg" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;">
 
         <div class="block"></div>
-        <div class="online_box_1"><img class="online_image_1" src="/assets/php/skin/Chazuke_0_.php" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;"><div class="online_name_1">Chazuke_0_</div><div class="online_state">Online</div></div>
-        <div class="online_box_2"><img class="online_image_2" src="/assets/php/skin/chishige1217200.php" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;"><div class="online_name_2">chishige1217200</div><div class="online_state">Online</div></div>
-        <div class="online_box_3"><img class="online_image_3" src="/assets/php/skin/fkddn123.php" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;"><div class="online_name_3">fkddn123</div><div class="online_state">Online</div></div>
-        <div class="online_box_4"><img class="online_image_4" src="/assets/php/skin/kit130101.php" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;"><div class="online_name_4">kit130101</div><div class="online_state">Online</div></div>
-        <div class="online_box_5"><img class="online_image_5" src="/assets/php/skin/meromin007.php" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;"><div class="online_name_5">meromin007</div><div class="online_state">Online</div></div>
-        <div class="online_box_6"><img class="online_image_6" src="/assets/php/skin/RyuMura.php" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;"><div class="online_name_6">RyuMura</div><div class="online_state">Online</div></div>
-        <div class="online_box_7"><img class="online_image_7" src="/assets/php/skin/suzumetengu.php" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;"><div class="online_name_7">suzumetengu</div><div class="online_state">Online</div></div>
-        <div class="online_box_8"><img class="online_image_8" src="/assets/php/skin/TUIUS.php" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;"><div class="online_name_8">TUIUS</div><div class="online_state">Online</div></div>
-        <div class="online_box_9"><img class="online_image_9" src="/assets/php/skin/Xx_piroboy_xX.php" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;"><div class="online_name_9">Xx_piroboy_xX</div><div class="online_state">Online</div></div>
-        <div class="online_box_10"><img class="online_image_10" src="/assets/php/skin/YuuYuu523.php" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;"><div class="online_name_10">YuuYuu523</div><div class="online_state">Online</div></div>
+        <div class="online_box_1"><img class="online_image_1" src="/assets/php/skin/Chazuke_0_.php" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;"><div class="online_name_1">Chazuke_0_</div><div id="online_state_1"></div></div>
+        <div class="online_box_2"><img class="online_image_2" src="/assets/php/skin/chishige1217200.php" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;"><div class="online_name_2">chishige1217200</div><div id="online_state_2"></div></div>
+        <div class="online_box_3"><img class="online_image_3" src="/assets/php/skin/fkddn123.php" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;"><div class="online_name_3">fkddn123</div><div id="online_state_3"></div></div>
+        <div class="online_box_4"><img class="online_image_4" src="/assets/php/skin/kit130101.php" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;"><div class="online_name_4">kit130101</div><div id="online_state_4"></div></div>
+        <div class="online_box_5"><img class="online_image_5" src="/assets/php/skin/meromin007.php" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;"><div class="online_name_5">meromin007</div><div id="online_state_5"></div></div>
+        <div class="online_box_6"><img class="online_image_6" src="/assets/php/skin/RyuMura.php" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;"><div class="online_name_6">RyuMura</div><div id="online_state_6"></div></div>
+        <div class="online_box_7"><img class="online_image_7" src="/assets/php/skin/suzumetengu.php" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;"><div class="online_name_7">suzumetengu</div><div id="online_state_7"></div></div>
+        <div class="online_box_8"><img class="online_image_8" src="/assets/php/skin/TUIUS.php" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;"><div class="online_name_8">TUIUS</div><div id="online_state_8"></div></div>
+        <div class="online_box_9"><img class="online_image_9" src="/assets/php/skin/Xx_piroboy_xX.php" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;"><div class="online_name_9">Xx_piroboy_xX</div><div id="online_state_9"></div></div>
+        <div class="online_box_10"><img class="online_image_10" src="/assets/php/skin/YuuYuu523.php" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;"><div class="online_name_10">YuuYuu523</div><div id="online_state_10"></div></div>
         <div class="block"></div>
     </main>
 

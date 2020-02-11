3a963b4a562fa833c9d76d690c08216b533ac994
smpny7 <symphonymode7@icloud.com>
2020-02-11 22:06:35 +0900

    history完成

--- assets/php/menu.php
+++ assets/php/menu.php
@@ -11,7 +11,7 @@
     <a class="menu_power" href="#"><img class="menu_icon_power" src="/assets/img/power.png" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;"><div class="menu_text_power">起動・終了</div></a><br>
     <a class="menu_restart" href="/restart"><img class="menu_icon_restart" src="/assets/img/restart.png" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;"><div class="menu_text_restart">再起動</div></a><br>
     <a class="menu_backup" href="#"><img class="menu_icon_backup" src="/assets/img/backup.png" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;"><div class="menu_text_backup">バックアップ実行</div></a><br>
-    <a class="menu_history" href="#"><img class="menu_icon_history" src="/assets/img/history.png" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;"><div class="menu_text_history">バックアップ履歴</div></a><br>
+    <a class="menu_history" href="/history"><img class="menu_icon_history" src="/assets/img/history.png" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;"><div class="menu_text_history">バックアップ履歴</div></a><br>
     <a class="menu_recovery" href="#"><img class="menu_icon_recovery" src="/assets/img/recovery.png" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;"><div class="menu_text_recovery">データ復元</div></a><br>
     <a class="menu_settings" href="#"><img class="menu_icon_settings" src="/assets/img/settings.png" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;"><div class="menu_text_settings">設定</div></a><br>
     </div>
--- history/index.php
+++ history/index.php
@@ -26,8 +26,22 @@
         <img class="background" src="/assets/img/background.jpg" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;">
 
         <div class="block"></div>
-        <div class="history_box_left"><img class="history_image" src="/assets/img/history.php" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;"><div class="history_date">2020/02/23</div><div class="history_time"></div></div>
-
+        <?php
+            $result = glob('./backup/20*');
+            // $result = glob('/home/jokura_banila/backup/20*');
+            $count = 1;
+            foreach (array_reverse($result) as $raw) {
+                if ($count % 2 == 1) {
+                    // echo("<div class=\"history_box_left\"><img class=\"history_image\" src=\"/assets/img/history2.png\" alt=\"画像\" oncontextmenu=\"return false;\" onselectstart=\"return false;\" onmousedown=\"return false;\"><div class=\"history_date\">".date('Y/m/d',strtotime(str_replace('_', ' ', ltrim($raw, './backup/'))))."</div><div class=\"history_time\">".date('H:i:s',strtotime(str_replace('_', ' ', ltrim($raw, './backup/'))))."</div></div>");
+                    echo("<div class=\"history_box_left\"><img class=\"history_image\" src=\"/assets/img/history2.png\" alt=\"画像\" oncontextmenu=\"return false;\" onselectstart=\"return false;\" onmousedown=\"return false;\"><div class=\"history_date\">".date('Y/m/d',strtotime(str_replace('_', ' ', ltrim($raw, '/home/jokura_banila/backup'))))."</div><div class=\"history_time\">".date('H:i:s',strtotime(str_replace('_', ' ', ltrim($raw, '/home/jokura_banila/backup'))))."</div></div>");
+                } else if($count % 2 == 0){
+                    // echo("<div class=\"history_box_right\"><img class=\"history_image\" src=\"/assets/img/history2.png\" alt=\"画像\" oncontextmenu=\"return false;\" onselectstart=\"return false;\" onmousedown=\"return false;\"><div class=\"history_date\">".date('Y/m/d',strtotime(str_replace('_', ' ', ltrim($raw, './backup/'))))."</div><div class=\"history_time\">".date('H:i:s',strtotime(str_replace('_', ' ', ltrim($raw, './backup/'))))."</div></div>");
+                    echo("<div class=\"history_box_right\"><img class=\"history_image\" src=\"/assets/img/history2.png\" alt=\"画像\" oncontextmenu=\"return false;\" onselectstart=\"return false;\" onmousedown=\"return false;\"><div class=\"history_date\">".date('Y/m/d',strtotime(str_replace('_', ' ', ltrim($raw, '/home/jokura_banila/backup'))))."</div><div class=\"history_time\">".date('H:i:s',strtotime(str_replace('_', ' ', ltrim($raw, '/home/jokura_banila/backup'))))."</div></div>");
+                }
+                $count++;
+                // <img class=\"history_delete\" src=\"/assets/img/delete.png\" alt=\"画像\" oncontextmenu=\"return false;\" onselectstart=\"return false;\" onmousedown=\"return false;\">
+            }
+        ?>
         <div class="block"></div>
     </main>
 

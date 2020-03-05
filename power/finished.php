81992a1fd7a82d001008890e09a807d8d745e732
smpny7 <symphonymode7@icloud.com>
2020-03-05 22:11:42 +0900

    power ローカル完成

--- power/finished.php
+++ power/finished.php
@@ -7,10 +7,12 @@
     }
 
     if($_SESSION["from"] == "powerOn") {
-        system("sudo -u jokura_banila bash /home/jokura_banila/minecraft/start.sh");
+        // system("sudo -u jokura_banila bash /home/jokura_banila/minecraft/start.sh");
+        echo("powerOn");
         $_SESSION["from"] = "";
     } elseif($_SESSION["from"] == "powerOff") {
-        system("sudo -u jokura_banila bash /home/jokura_banila/minecraft/stop.sh");
+        // system("sudo -u jokura_banila bash /home/jokura_banila/minecraft/stop.sh");
+        echo("powerOff");
         $_SESSION["from"] = "";
     } else {
         header('Location: /');
@@ -38,11 +40,11 @@
         <img class="background" src="/assets/img/background.jpg" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;">
 
         <div class="block"></div>
-        <div class="restart_box">
-            <div class="restart_title">サーバ停止</div>
-            <img class="restart_img" src="/assets/img/power2.png" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;">
-            <div class="restart_contents">サーバに処理を送信しました．<br>まもなくサーバが停止されます．</div>
-            <a href="/"><img class="restart_yes" src="/assets/img/yes.png" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;"></a>
+        <div class="power_box">
+            <div class="power_title">サーバ停止</div>
+            <img class="power_img" src="/assets/img/power2.png" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;">
+            <div class="power_contents">サーバに処理を送信しました．<br>まもなくサーバが停止されます．</div>
+            <a href="/"><img class="power_yes" src="/assets/img/yes.png" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;"></a>
         </div>
         <div class="block"></div>
     </main>
--- power/index.php
+++ power/index.php
@@ -5,8 +5,6 @@
         header('Location: /maintenance');
         exit;
     }
-
-    $_SESSION["from"] = "powerOn";
 ?>
 
 <!DOCTYPE html>
@@ -30,10 +28,12 @@
 
         <div class="block"></div>
         <div class="power_box">
-            <div class="power_title"><?php include '../assets/php/power.php'; if(power()==1){echo "サーバ停止";}else{echo "サーバ起動";};?></div>
+            <div class="power_title"><?php include '../assets/php/power.php'; if(power()==1){echo "サーバ停止";$_SESSION["from"] = "powerOff";}else{echo "サーバ起動";$_SESSION["from"] = "powerOn";}?></div>
             <img class="power_img" src="/assets/img/power2.png" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;">
-            <div class="power_contents">ボタン押下後，30秒程度かかりますが<br>そのままお待ちください．</div>
-            <img class="power_yes" src="/assets/img/yes.png" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;">
+            <div class="power_contents">ボタン押下後，<?php if(power()==1){echo "30";}else{echo "10";}?>秒程度かかりますが<br>そのままお待ちください．</div>
+            <a id="submit" href="finished.php"><img id="yes" src="/assets/img/yes.png" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;"></a>
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+                <script type="text/javascript" src="/assets/js/doubleTap.js"></script>
             <a href="/"><img class="power_no" src="/assets/img/no.png" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;"></a>
         </div>
         <div class="block"></div>

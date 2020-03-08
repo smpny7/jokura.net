a7bec2965aaccce8f07b1ec22da7cdb271211e23
smpny7 <symphonymode7@icloud.com>
2020-03-08 21:31:53 +0900

    restart finished 新コード & processReject 途中

--- a.php
+++
@@ -1,4 +0,0 @@
-<?php
-    require __DIR__ . '/assets/php/check/processCheck.php';
-    echo processCheck();
-?>
--- processReject/index.php
+++ processReject/index.php
@@ -1,22 +1,6 @@
 <?php
     require __DIR__ . '/../assets/php/check/maintenanceCheck.php';
     maintenanceCheck();
-
-    session_start();
-    if($_SESSION["from"] == "restart") {
-        require __DIR__ . '/../assets/php/check/processCheck.php';
-        if (processCheck()) {
-            readfile("http://jokura-vanila.work/src/restart.php?from=jokura.net");
-        } else {
-            $_SESSION["from"] = "";
-            header('Location: /processReject');
-            exit;
-        }
-    } else {
-        $_SESSION["from"] = "";
-        header('Location: /');
-        exit;
-    }
 ?>
 
 <!DOCTYPE html>
@@ -25,7 +9,7 @@
     <meta charset="UTF-8">
     <meta name="robots" content="noindex">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>サーバ再起動</title>
+    <title>プロセス拒否</title>
     <link rel="apple-touch-icon" sizes="180x180" href="/assets/icon/apple-touch-icon.png">
     <link rel="icon" href="/assets/icon/icon.ico">
     <link rel="stylesheet" href="/assets/css/reset.css">
@@ -41,10 +25,11 @@
 
         <div class="block"></div>
         <div class="restart_box">
-            <div class="restart_title">サーバ再起動</div>
+            <div class="restart_title">プロセス拒否</div>
             <img class="restart_img" src="/assets/img/restart2.png" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;">
             <div class="restart_contents">サーバに処理を送信しました。<br>まもなく再起動が実行されます。</div>
             <a href="/"><img class="restart_yes" src="/assets/img/yes.png" alt="画像" oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;"></a>
+        </div>
         </div>
         <div class="block"></div>
     </main>
